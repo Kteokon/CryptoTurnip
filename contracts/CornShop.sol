06d2d5d5724f1cc07b1bf551b2b8687bc7360a99
lisugargirl <lisugargrl@gmail.com>
2023-01-12 00:43:58 +0800

    add Bank and Store contracts

--- contracts/CornShop.sol
+++ contracts/CornShop.sol
@@ -3,30 +3,30 @@ pragma solidity >=0.5.0 <0.6.0;
 import "../adds/safemath.sol";
 
 contract CornShop {
-    using SafeMath for uint256;
-    using SafeMath32 for uint32;
-    using SafeMath16 for uint16;
+    // using SafeMath for uint256;
+    // using SafeMath32 for uint32;
+    // using SafeMath16 for uint16;
 
-    event NewShop(uint shopId, uint pattern);
+    // event NewShop(uint shopId, uint pattern);
 
-    uint patternDigits = 16;
-    uint patternModulus = 10 ** patternDigits;
+    // uint patternDigits = 16;
+    // uint patternModulus = 10 ** patternDigits;
 
-    struct Shop {
-        uint inventorySize;
-        uint pattern;
-        bool firstBought;
-    }
+    // struct Shop {
+    //     uint inventorySize;
+    //     uint pattern;
+    //     bool firstBought;
+    // }
 
-    Shop[] shops;
+    // Shop[] shops;
 
-    mapping (uint => address) public shopToOwner;
-    mapping (address => uint) ownerShopCount;
+    // mapping (uint => address) public shopToOwner;
+    // mapping (address => uint) ownerShopCount;
 
-    function createShop() public {
-        uint id = shops.push(Shop(8, 0, false)) - 1;
-        shopToOwner[id] = msg.sender;
-        ownerShopCount[msg.sender] = ownerShopCount[msg.sender].add(1);
-        emit NewShop(id, 0);
-    }
+    // function createShop() public {
+    //     uint id = shops.push(Shop(8, 0, false)) - 1;
+    //     shopToOwner[id] = msg.sender;
+    //     ownerShopCount[msg.sender] = ownerShopCount[msg.sender].add(1);
+    //     emit NewShop(id, 0);
+    // }
 }
