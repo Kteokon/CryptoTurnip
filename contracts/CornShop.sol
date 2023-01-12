50832c3b510f766957890711a18b6383cef39bcf
Anastasia Maksimova <ktoenok0801@gmail.com>
2023-01-12 20:44:00 +0800

    Trying to merge Shop and Store

--- contracts/CornShop.sol
+++ contracts/CornShop.sol
@@ -27,7 +27,7 @@ contract CornShop is CornStore {
         changePattern();
     }
 
-    function buyCorn(uint amountOfCorn) public payable { // фукнция покупки кукурузы
+    function buyCorn(uint amountOfCorn) public payable { // Функция покупки кукурузы
         require(msg.value == amountOfCorn * price);
         uint _storeId = CornStore.ownerToStore[msg.sender];
         putCorn(_storeId, amountOfCorn);
@@ -41,34 +41,7 @@ contract CornShop is CornStore {
     function generateBoughtPrice() private { // Цена покупки кукурузы варьируется от 0.001 до 0.002
         price = (_generateRandomNum(100) + 100) * coeff;
     }
-    function generateBoughtPrice() private { // Цена покупки кукурузы варьируется от 0.001 до 0.002
-        price = (_generateRandomNum(100) + 100) * coeff;
-    }
 
-    function generateSellPrice() private  { // Цена продажи зависит от паттерна
-        uint date = block.timestamp + 8 hours; // День покупки
-        date = date - (date % 1 days);
-        require(date - block.timestamp >= 1 hours);
-        LastUpdate = date;
-        if (_compareStrings(pattern, "random")) {
-            price = (_generateRandomNum(110) + 90) * coeff;
-        }
-        else {
-            if (_compareStrings(pattern, "small")) {
-                price = (_generateRandomNum(260) + 90) * coeff;
-            }
-            else {
-                if (_compareStrings(pattern, "big")) {
-                    price = (_generateRandomNum(410) + 90) * coeff;
-                }
-                else {
-                    if (_compareStrings(pattern, "decreasing")) {
-                        price = (_generateRandomNum(70) + 30) * coeff;
-                    }
-                }
-            }
-        }
-    }
     function generateSellPrice() private  { // Цена продажи зависит от паттерна
         uint date = block.timestamp + 8 hours; // День покупки
         date = date - (date % 1 days);
@@ -106,34 +79,7 @@ contract CornShop is CornStore {
                 }
             }
         }
-    function changePattern() public {
-        if (!_compareStrings(pattern, "nothing")) { // Отнимаем вероятность у паттерна, который был на этой неделе
-            for (uint i = 0; i < 4; i++) {
-                string memory p = idToPattern[i];
-                if (_compareStrings(pattern, p)) {
-                    patternToPrecent[pattern] -= 9;
-                }
-                else {
-                    patternToPrecent[p] += 3;
-                }
-            }
-        }
 
-        uint rand = _generateRandomNum(100); // Случайный выбор паттерна
-        for (uint i = 0; i < 4; i++) {
-            string memory p = idToPattern[i];
-            uint pr = patternToPrecent[p];
-            if (pr < rand) {
-                rand -= pr;
-            }
-            else {
-                pattern = p;
-                break;
-            }
-        }
-        precentsToNormal();
-        generateSellPrice();
-    }
         uint rand = _generateRandomNum(100); // Случайный выбор паттерна
         for (uint i = 0; i < 4; i++) {
             string memory p = idToPattern[i];
@@ -156,25 +102,12 @@ contract CornShop is CornStore {
         patternToPrecent["big"] = 25;
         patternToPrecent["decreasing"] = 15;
     }
-    function precentsToNormal() private {
-        patternToPrecent["random"] = 35;
-        patternToPrecent["small"] = 25;
-        patternToPrecent["big"] = 25;
-        patternToPrecent["decreasing"] = 15;
-    }
 
     function _generateRandomNum(uint _modulus) internal returns (uint) {
         randNonce++;
         return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
     }
-    function _generateRandomNum(uint _modulus) internal returns (uint) {
-        randNonce++;
-        return uint(keccak256(abi.encodePacked(now, msg.sender, randNonce))) % _modulus;
-    }
 
-    function _compareStrings(string memory a, string memory b) private pure returns (bool) {
-        return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
-    }
     function _compareStrings(string memory a, string memory b) private pure returns (bool) {
         return (keccak256(abi.encodePacked((a))) == keccak256(abi.encodePacked((b))));
     }
